c805611a21345ce585fbb999b27dabfdc1148421
Trevor Youngquist <ytrevor81@gmail.com>
2021-10-31 16:25:38 +0700

    ideas for nft contracts

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -2,13 +2,58 @@
 pragma solidity >=0.6.0 <0.9.0;
 
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
 
 contract FundMe {
 
-  mapping(address => uint256) public addressToAmountFunded;
+    using SafeMathChainlink for uint256;
 
-  function fund() public payable {
-    addressToAmountFunded[msg.sender] += msg.value;
-  }
+    mapping(address => uint256) public addressToAmountFunded;
+    address[] public funders;
+    address internal owner;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    function fund() public payable {
+        uint256 minimumUSD = 50 * 10 ** 18;
+        require(getConversionRate(msg.value) >= minimumUSD, "not enough ETH"); //same as if, but better practice for solidity
+
+        addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
+    }
+
+    function getVersion() public view returns(uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        return priceFeed.version();
+    }
+
+    function getPrice() public view returns(uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        (,int256 answer,,,)
+          = priceFeed.latestRoundData();
+          return uint256(answer * 10000000000); //doing this everytime will cost more gas than needed
+    }
+
+    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUSD = (ethPrice * ethAmount) / 1000000000000000000;
+        return ethAmountInUSD;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function withdraw() payable onlyOwner public {
+        msg.sender.transfer(address(this).balance);
+        for (uint256 funderIndex=0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+    }
 
 }
