0dccb3d78abf869445a767651b8b24735643684c
Trevor Youngquist <ytrevor81@gmail.com>
2022-01-28 18:33:07 +0700

    DAO contracts in the works

--- contracts/DAOProject/DeanDAO.sol
+++ contracts/DAOProject/DeanDAO.sol
@@ -6,37 +6,38 @@ import "./IDeanDAO.sol";
 
 contract DeanDAO is IDeanDAO {
 
-  //DeanToken public immutable acceptedTokenAddress;
-  address public immutable acceptedTokenAddress;
-  string public immutable _name;
+  address public acceptedTokenAddress;
+  string public _name = "DeanDAO";
+  address public governor;
+  address public executor;
+
   uint256 private immutable valueOfEachVote;
-
   uint256 private _amountOfDTK;
-
-  address private governor;
-  address private executor;
-
   uint256 private proposalIDCounter;
   uint256 private quorum;
 
   enum ProposalState { Active, Cancelled, Defeated, Suceeded, Expired }
 
   struct Proposal {
-    uint256 proposalID,
-    string description,
-    address proposer,
-    uint256 votesFor,
-    uint256 votesAgainst,
-    uint256 dateProposed,
-    uint256 deadline,
-    ProposalState state
-  };
+    uint256 proposalID;
+    string description;
+    address proposer;
+    uint256 votesFor;
+    uint256 votesAgainst;
+    uint256 numberOfVotes;
+    uint256 dateProposed;
+    uint256 deadline;
+    ProposalState state;
+  }
 
   struct Vote {
-    address voter,
-    uint256 proposalID,
-    bool forOrAgainst,
-  };
+    address voter;
+    uint256 proposalID;
+    bool support;
+  }
+
+  Vote[] votes;
+  Proposal[] proposals;
 
   //address mapped to specific vote
   mapping (address => Vote) private submittedVotes;
@@ -45,15 +46,11 @@ contract DeanDAO is IDeanDAO {
   mapping (uint256 => bool) private proposalMetQuorum;
 
   //returns how many votes in total a proposal has received
-  mapping (uint256 proposalID => uint256 numberOfVotes) private numberOfVotesForProposal;
-
-  Vote private votes[];
-  Proposal private proposals[];
+  mapping (uint256 => uint256) private numberOfVotesForProposal;
 
   constructor(address _deanTokenAddress, uint256 _quorum) {
     acceptedTokenAddress = _deanTokenAddress;
     governor = msg.sender;
-    _name = "DeanDAO"
     proposalIDCounter = 0;
     quorum = _quorum;
     valueOfEachVote = 1 * 10 **18; //1 token is 1 vote
@@ -92,8 +89,7 @@ contract DeanDAO is IDeanDAO {
   event VoteSubmitted (
     address indexed voter,
     uint256 proposalID,
-    bool forOrAgainst,
-    string reason
+    bool support,
     );
 
   event ReturnVoteTokens (
@@ -109,16 +105,19 @@ contract DeanDAO is IDeanDAO {
 
   //only allows DTK holders
   modifier onlyDTKHolder() {
-    require(DeanToken.balanceOf(msg.sender) > 0, "Only DeanToken holders can participate");
+    require(IERC20(acceptedTokenAddress).balanceOf(msg.sender) > 0, "Only DeanToken holders can participate");
+    _;
   }
   //only allows the governor address
   modifier onlyGovernor() {
     require(msg.sender == governor, "Only the governor can call this function");
+    _;
   }
 
   //only allows the proposal executor address
   modifier onlyExecutor() {
     require(msg.sender == executor, "Only the executor can call this function");
+    _;
   }
 
   /**
@@ -126,17 +125,7 @@ contract DeanDAO is IDeanDAO {
   **/
 
   //Returns the governor address of the DAO
-  function governor() external view returns (string memory) {
-    return governor;
-  }
-
-  //Returns the governor address of the DAO
-  function executor() external view returns (string memory) {
-    return executor;
-  }
-
-  //Returns the governor address of the DAO
-  function name() external view returns (string memory) {
+  function name() public view returns (string memory) {
     return _name;
   }
 
@@ -151,14 +140,14 @@ contract DeanDAO is IDeanDAO {
   }
 
   //Returns the state of a proposal
-  function viewProposal(uint256 _proposalID) external view onlyDTKHolder returns (Proposal) {
-    Proposal proposal = Proposals[_proposalID];
+  function viewProposal(uint256 _proposalID) external view onlyDTKHolder returns (Proposal memory) {
+    Proposal memory proposal = proposals[_proposalID];
     return proposal;
   }
 
   //Returns a specific vote from a voter
-  function viewVote(address _user) external view onlyDTKHolder returns (Vote) {
-    Vote vote = submittedVotes[_user];
+  function viewVote(address _user) external view onlyDTKHolder returns (Vote memory) {
+    Vote memory vote = submittedVotes[_user];
     return vote;
   }
 
@@ -166,44 +155,51 @@ contract DeanDAO is IDeanDAO {
     * Voting functions:
   **/
 
-  function determineVoteForOrAgainst(Vote _vote) internal pure returns(bool) {
-    return _vote.forOrAgainst;
-  };
+  function determineVoteForOrAgainst(Vote memory _vote) internal pure returns(bool) {
+    return _vote.support;
+  }
 
   function voterAlreadyVotedOnProposal(address _user) internal returns (bool) {
     if (submittedVotes[_user].proposalID != 0) {
       return true;
-    };
+    }
     return false;
-  };
+  }
 
-  function voteAssignedToProposal(Vote _vote) internal {
-    Proposal proposal = Proposals[_vote.proposalID];
+  function voteAssignedToProposal(Vote memory _vote) internal {
+    proposals[_vote.proposalID].numberOfVotes = proposals[_vote.proposalID].numberOfVotes + 1;
 
+    if (proposals[_vote.proposalID].numberOfVotes >= quorum) {
+      proposalMetQuorum[proposals[_vote.proposalID].proposalID] = true;
+    }
 
-    emit VoteSubmitted();
-  };
+    if (_vote.support) {
+      proposals[_vote.proposalID].votesFor = proposals[_vote.proposalID].votesFor + 1;
+    }
+    else {
+      proposals[_vote.proposalID].votesAgainst = proposals[_vote.proposalID].votesAgainst + 1;
+    }
+  }
 
   function receiveVotingToken(address _sender) internal {
     IERC20(acceptedTokenAddress).transferFrom(_sender, address(this), valueOfEachVote);
     _amountOfDTK += valueOfEachVote;
-  };
+  }
 
   function castVote(address tokenAddress, uint256 _proposalID, bool _support) external payable onlyDTKHolder returns (bool) {
     address _sender = msg.sender;
-
     require(tokenAddress == acceptedTokenAddress, "We only except Dean Tokens (DTK) for voting");
     require(msg.value == valueOfEachVote, "Only 1 DeanToken is excepted as a vote for or against a proposal");
-    require(submittedVotes[_sender].proposalID != 0, "User has already voted on this proposal");
+    require(submittedVotes[_sender].voter != _sender, "User has already voted on this proposal");
+
     receiveVotingToken(_sender);
 
-    Vote newVote = Vote(_sender, _proposalID, _support);
+    Vote memory newVote = Vote(_sender, _proposalID, _support);
+    submittedVotes[_sender] = newVote;
     voteAssignedToProposal(newVote);
 
-    Proposal proposal = Proposals[_proposalID];
-
-
-    returns true;
+    emit VoteSubmitted(newVote.voter, newVote.proposalID, newVote.support);
+    return true;
   }
 
   /**
@@ -213,7 +209,7 @@ contract DeanDAO is IDeanDAO {
   function proposalHasMetQuorum(uint256 _proposalID) external view override returns (bool) {
     bool proposalCanConsidered = proposalMetQuorum[_proposalID];
     return proposalCanConsidered;
-  };
+  }
 
   function submitProposal(string calldata _description, uint256 _deadline) external onlyDTKHolder returns (bool success) {
     address proposer = msg.sender;
@@ -221,7 +217,7 @@ contract DeanDAO is IDeanDAO {
     uint256 deadline = block.timestamp + _deadline;
     ProposalState state = ProposalState.Active;
 
-    Proposal newProposal = Proposal(proposalIDCounter, description, proposer, dateProposed, deadline, state);
+    Proposal memory newProposal = Proposal(proposalIDCounter, _description, proposer, dateProposed, deadline, state);
     proposals.push(newProposal);
 
     proposalIDCounter = proposalIDCounter + 1;
@@ -238,19 +234,19 @@ contract DeanDAO is IDeanDAO {
   }
 
   function proposalDefeated(uint256 _proposalID) external onlyExecutor override returns (bool) {
-    Proposal defeatedProposal = proposals[_proposalID];
+    Proposal memory defeatedProposal = proposals[_proposalID];
     defeatedProposal.state = ProposalState.Defeated;
 
     emit ProposalDefeated(defeatedProposal.proposalID, defeatedProposal.description, defeatedProposal.state);
-    returns true;
+    return true;
   }
 
-  function proposalSucceeded() external onlyExecutor override returns (bool) {
-    Proposal succeededProposal = proposals[_proposalID];
+  function proposalSucceeded(uint256 _proposalID) external onlyExecutor override returns (bool) {
+    Proposal memory succeededProposal = proposals[_proposalID];
     succeededProposal.state = ProposalState.Suceeded;
 
-    emit ProposalSucceeded(succeededProposal.proposalID, succeededProposal.description, succeededProposal.state);
-    returns true;
+    emit ProposalExecuted(succeededProposal.proposalID, succeededProposal.description, succeededProposal.state);
+    return true;
   }
 
   /**
